303a2d97d4a03f5ce3d7f5b45ec1108d7eb77a83
Anastasia Sazonova <anastasia.ilyukovich@pandadoc.com>
2022-05-20 22:10:41 +0200

    feat: implement ability to copy files from one dir to another

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
+const path = require('path');
 
+const fsPromises = fs.promises;
+const duplicatedDirPath = path.join(__dirname, 'fs_test2');
+const originalDirPath = path.join(__dirname, 'files');
+
+fsPromises.mkdir(duplicatedDirPath, {recursive: true}).then(() => {
+  console.log('Directory created successfully');
+  fs.readdir(originalDirPath, async (_, files) => {
+    for (const file of files) {
+      const oldFilePath = path.join(__dirname, 'files', file);
+      const newFilePath = path.join(__dirname, 'fs_test2', file);
+
+      try {
+        await fs.copyFile(oldFilePath, newFilePath, (err) => {
+          if (err) {
+            console.log(err);
+          }
+        });
+      } catch {
+        console.log('The file could not be copied');
+      }
+    }
+  });
+});
